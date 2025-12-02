4b3d437cf8d5adcbc8f52c14e020c019246b8761
nam-joshi <namrata.joshi1998@gmail.com>
2025-12-02 14:46:09 -0800

    Test: trigger AI TestGen bot run #1

--- cypress/e2e/csuf_homepage.cy.js
+++ cypress/e2e/csuf_homepage.cy.js
@@ -3,7 +3,7 @@ describe('CSUF Homepage Smoke Test', () => {
       cy.visit('https://www.fullerton.edu/');
     });
 
-    it("checks that the search bar is visible on homepage", () => {
+    it("checks that the search bar is visible", () => {
         cy.visit("https://www.fullerton.edu");
         cy.get("input[type='search'], #search, .search").should("exist");
       });
