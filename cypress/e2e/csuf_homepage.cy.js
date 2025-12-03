192bc3f29b2574cda52ce5fd8ebae18f01e35847
nam-joshi <namrata.joshi1998@gmail.com>
2025-12-02 20:33:59 -0800

    Retrigger AI bot

--- cypress/e2e/csuf_homepage.cy.js
+++ cypress/e2e/csuf_homepage.cy.js
@@ -3,7 +3,7 @@ describe('CSUF Homepage Smoke Test', () => {
       cy.visit('https://www.fullerton.edu/');
     });
 
-    it("checks that the search bar is visible", () => {
+    it("Search bar functionality", () => {
         cy.visit("https://www.fullerton.edu");
         cy.get("input[type='search'], #search, .search").should("exist");
       });
